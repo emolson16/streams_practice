72080e4f5e3358551552d3b50bacc96df919a96a
Emily Olson <61092896+emolson16@users.noreply.github.com>
2021-10-05 17:14:51 -0400

    Add files via upload

---
+++ computeAverage.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <iostream>
+#include <string>
+#include <fstream>
+
+// file name will be given in the command line
+int main(int argc, char * argv[]) {
+    // TODO:
+    // Error check argc
+    // Open file and error check
+    // Read in data (error check as you go) and calculate average
+    // Write average to cout
+    // Close file
+
+    
+    return 0;
+}
